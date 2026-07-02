b4ce791f9b0082bb422b5db5a25b8e43f3624d77
Presk0o <mathisdelassuspro@gmail.com>
2026-07-02 23:21:43 +0200

    Update local CRM launcher

--- start-crm.bat
+++ start-crm.bat
@@ -1,10 +1,10 @@
 @echo off
 cd /d "%~dp0"
-echo Kroma CRM local
+echo Kroma HQ CRM local
 echo.
-echo Serveur : http://localhost:3000/#dashboard
+echo Serveur : http://localhost:3000
 echo Ferme cette fenetre pour arreter le CRM.
 echo.
-start "" powershell -NoProfile -WindowStyle Hidden -Command "Start-Sleep -Seconds 2; Start-Process 'http://localhost:3000/#dashboard'"
+start "" powershell -NoProfile -WindowStyle Hidden -Command "Start-Sleep -Seconds 2; Start-Process 'http://localhost:3000'"
 node server.js
 pause
